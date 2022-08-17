80aff61cb8de6d443743cb8e1c984866a8db51b3
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-17 15:01:27 +0900

    Deploy in heroku cli in heroku remote branch

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join('client/build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
