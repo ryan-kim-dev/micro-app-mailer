52fdbefa06da349f3976f8c268aebec97e380277
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-17 12:09:41 +0900

    Trigger Deploy

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-// import { axiosInstance } from '../config';
+import { axiosInstance } from '../config';
 const App = () => {
   const [data, setData] = useState({
     yourname: '',
@@ -25,7 +25,7 @@ const App = () => {
   const onSubmit = async e => {
     e.preventDefault();
     axios
-      .post('/mail', {
+      .post(`${axiosInstance}/mail`, {
         data: {
           ...data,
         },
