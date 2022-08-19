ed3c37e61bfa9c44f13e8e019369f72557c12cdf
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-20 00:06:05 +0900

    chore: Fix then-catch callbacks in POST response

--- index.js
+++ index.js
@@ -40,20 +40,15 @@ app.get('/', (req, res) => {
 app.post('/mail', async (req, res) => {
   const { name, email, subject, message } = req.body.data;
 
-  main(name, email, subject, message).then(response => {
-    if (response === 'success') {
+  main(name, email, subject, message)
+    .then(response => {
       res.status(200).json({
         status: 'Success',
         code: 200,
         message: '메일이 성공적으로 발송되었습니다.',
       });
-    } else {
-      res.json({
-        status: 'Fail',
-        code: response.code,
-      });
-    }
-  });
+    })
+    .catch(err => console.log(`${err}`));
 });
 
 app.listen(port, () => {
